2520ce5cb6ebc22bf753b46865c15ebb7a7c6648
C. M. McColeman <cmccolem@sfu.ca>
2018-11-15 11:22:53 -0600

    updates

---
+++ VTLStudy/redundantCoding/drawStims.m
@@ -0,0 +1,30 @@
+function drawStims(setSize, proportion, redundancy, centroid)
+%
+%  Author: Caitlyn McColeman 
+%  Date Created: February 26 2018
+%  Last Edit:    November 8 2018
+%  
+%  Visual Thinking Lab, Northwestern University
+%  Originally Created For: Perception, Priors and Cognition redundant coding study.
+%  
+%  Reviewed: [] 
+%  Verified: [] 
+%  
+%  INPUT: 
+%       setSize, integer; the number of icons in the array
+%        proportion, int; how many make up the larger set.
+%    redundancy, logical; are the groups redundantly encoded? 
+%         centroid, vect; the x, y coordinates for the center of the array.
+%  
+%  OUTPUT: 
+%
+%  EXAMPLE:
+%       
+%  
+%  Additional Scripts Used: 
+%  
+%  Additional Comments: 
+
+
+
+% 1. Determine how many icons there .
