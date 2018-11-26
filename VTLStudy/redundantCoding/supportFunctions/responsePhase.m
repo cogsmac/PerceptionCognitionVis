dd1418f83b4aa3cda2ce5afc4f2b9db443011fd4
C. M. McColeman <cmccolem@sfu.ca>
2018-11-26 02:39:43 -0600

    little usability tweaks

--- VTLStudy/redundantCoding/supportFunctions/responsePhase.m
+++ VTLStudy/redundantCoding/supportFunctions/responsePhase.m
@@ -1,7 +1,7 @@
 %  This is a wrapper function to contain all of the code and functions 
 %  relevant for making a response on the response bar. 
 %
-function [responseOnset, responseTime, responsePixels, responseRatio] = responsePhase(kbPtr, windowPtr)
+function [responseTime, responsePixels, responseRatio] = responsePhase(kbPtr, windowPtr, screenXMax, screenYMax)
 %
 %  Author: Caitlyn McColeman
 %  Date Created: November 19 2018
@@ -21,9 +21,6 @@ function [responseOnset, responseTime, responsePixels, responseRatio] = response
 %
 %  Additional Comments:
 
-global screenXpixels 
-global screenYpixels
-
 % some hard-coded constants we can adjust on additional iterations
      scaleType = 'rectangleAcross'; % starting with this for now; we can change to be a variable if we want to play with other response options.
 responseBarCol = [0,0,0];           % what color is the participant using to draw?
@@ -38,18 +35,23 @@ commandwindow;          % coerce cursor to command window for easy exit and to k
 % set-up properties of the response scale outline 
 switch lower(scaleType)
     case 'rectangleacross' %[todo make a choice whether to use color here too if it's meaningful during stimulus phase]
-        minX = .25 * screenXpixels;
-        maxX = .75 * screenXpixels;
+        minX = .25 * screenXMax;
+        maxX = .75 * screenXMax;
         
-        minY = .66 * screenYpixels;
-        maxY = .75 * screenYpixels;
+        minY = .66 * screenYMax;
+        maxY = .75 * screenYMax;
         
         snapVec = [minY maxY]; % participants won't draw vertically, so we'll do it for them    
+
+        previousRect = [minX minY minX+1 minY+1];  %initialize to something silly, this will store the participants' latest response so they can see it while they adjust the bar
+
 end
 
 weHaveSomethingToDraw = 0; %initially, but as the user adjusts the scale we'll have their last response to draw in
-previousRect = [0 0 1 1];  %initialize to something silly, this will store the participants' latest response so they can see it while they adjust the bar
 responseAdjustmentRec = [];%in case we'd like to see how people adjust the response bar over time, we can access it from a .mat file saved herein
+keycode = 0;               %no button press to start, but listen until there is via while loop
+mouseSampler = 0;
+
 
 
 while sum(keycode)==0   % present response scale, and wait for the user to press enter to advance.
@@ -62,14 +64,15 @@ while sum(keycode)==0   % present response scale, and wait for the user to press
     [x,y,buttons,focus,valuators,valinfo] = GetMouse();
    
     % draw the outline of the response scale 
-    Screen(windowPtr,'FrameRect',[0,0,0],[minX, minY, maxX, maxY],2,2)
+    Screen('FrameRect',windowPtr, [0 0 0],[minX, minY, maxX, maxY])
     
     % determine if participants are hovering/clicking over the response window
-    inAdjustmentRegion = x>minX-30 && x<minX+30; % an extra 30 pixels added as a usability buffer
+    inAdjustmentRegion = y>minY-30 && y<maxY+30; % an extra 30 pixels added as a usability buffer
             buttonDown = sum(buttons)>0;         % is the mouse clicked?
     
     [hasBeenAdjusted, updatedRect] = responseScale(scaleType, inAdjustmentRegion, weHaveSomethingToDraw, buttonDown, previousRect, x, y, windowPtr, responseBarCol, snapVec);
     
+                      previousRect = updatedRect; % replace the previously stored rectangle with most recent reponse
     % note whether there's a response so we make sure to draw in the on the
     % next iteration
     weHaveSomethingToDraw = hasBeenAdjusted;
@@ -79,17 +82,14 @@ while sum(keycode)==0   % present response scale, and wait for the user to press
     if weHaveSomethingToDraw
         mouseSampler = mouseSampler + 1; % keep track of iterations
         
-        adjustOnset(mouseSampler) = Screen('Flip', windowPtr, stimulus1Offset + (waitframes - 0.5) * ifi);
+        %adjustOnset(mouseSampler) = Screen('Flip', windowPtr);
         % Horizontally and vertically centered:
         [nx, ny, bbox] = DrawFormattedText(windowPtr, instructionTxt, 'center', 'center', 0);
         
         % record the value of the drawn rectangle this millisecond
         responseAdjustmentRec = [responseAdjustmentRec updatedRect];
     end
-    
-    % push everything to screen 
-    responseOnset = Screen('Flip', windowPtr);
-    
+    Screen('Flip', windowPtr)
     WaitSecs(0.001); % avoid overloading the system on checks, update keyboard and mouse status each millisecond
     
 end
@@ -97,4 +97,4 @@ end
 % variables for output
  responseTime = toc(responseOnsetTic);             % how long from the start to the end of the response phase
 responsePixels= updatedRect(3)-minX;               % how many pixels did the user draw in?
-responseRatio = responsePixels/(diff(maxX, minX)); % what percentage was the user estimate?
+responseRatio = responsePixels/(maxX-minX); % what percentage was the user estimate?
